cf6d10156bf7542051b53265651af295044db420
Muhammad Ibrahim <foraptech080@gmail.com>
2025-01-27 07:57:23 +0500

    Last#

--- js/script.js
+++ js/script.js
@@ -55,3 +55,20 @@ window.onresize = function () {
     }
   }
 };
+
+
+// ANIMATIONS 
+
+var observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('showw')
+    } else {
+      entry.target.classList.remove('showw')
+    }
+  });
+});
+
+var hiddenElements = document.querySelectorAll('.item')
+hiddenElements.forEach((el) => observer.observe(el));
